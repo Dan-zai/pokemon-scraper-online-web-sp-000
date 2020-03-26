2e889312f1f7a41d5a71f3e0a38ee73d406fafae
Daniel Cowpertwait <danielcowpertwait@gmail.com>
2020-03-26 06:09:04 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -8,15 +8,17 @@ class Pokemon
     @type = type
     @db = db
   end
+  
+  def self.find(id, db)
+    #as it's one line, no need to write sql = HDOCs for excecution
+    pokemon_from_db = db.execute("SELECT * FROM pokemon WHERE id = ?;", id).flatten
+    Pokemon.new(id: pokemon_from_db[0], name: pokemon_from_db[1], type: pokemon_from_db[2], db: db)
+  end
 
   def self.save(name, type, db)
     db.execute("INSERT INTO pokemon (name, type) VALUES(?, ?);", name, type )
   end
 
-  def self.find(id, db)
-    pokemon_from_db = db.execute("SELECT * FROM pokemon WHERE id = ?;", id).flatten
-    Pokemon.new(id: pokemon_from_db[0], name: pokemon_from_db[1], type: pokemon_from_db[2], db: db)
-  end
   
   
 end
